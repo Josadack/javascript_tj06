28206e4764c2d6a7068ce1fc749ca0d84fa04d7b
unknown <josadaqueferreira@outlook.com>
2024-12-05 19:58:50 -0300

    Revert "Aula 04 - Laços de Repetição"
    This reverts commit a90ad8c4fa395c580c05b65c6fe60c8985780586.

--- aula_04/Tabuada.js
+++
@@ -1,7 +0,0 @@
-const leia = require('readline-sync');
-
-let numero = leia.questionInt('Digite um numero: ');
-
-for(let contador =1; contador <= 10; contador++){
-    console.log(`${numero} X ${contador} = ${numero * contador}`) 
-}
