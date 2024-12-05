92e1bf15b3b4c85828619021bfe3c000c6774586
Dack Ferreira <68622187+Josadack@users.noreply.github.com>
2024-12-05 20:46:05 -0300

    Delete aula_04 directory

--- aula_04/TabuadaDoWhile.js
+++
@@ -1,9 +0,0 @@
-const leia = require('readline-sync');
-
-let numero = leia.questionInt('Digite um numero: ');
-let contador = 1
-
-do{
-    console.log(`${numero} X ${contador} = ${numero * contador}`);
-    contador ++;
-}while(contador <= 10)
